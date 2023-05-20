dfd77027f338c3ac253bfce412c86e050338c3d1
Lucas Menduiña <mynameis_menduina@outlook.com>
2023-05-20 12:54:08 -0300

    feat: hourContract class

---
+++ Heranca/Contratos/src/entities/ContratoHora.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Date;
+
+public class ContratoHora {
+	private Date date ; 
+	private Double valorHora ; 
+	private Integer horas ; 
+	
+	public ContratoHora() {
+		
+	}
+
+	public ContratoHora(Date date, Double valorHora, Integer horas) {
+		this.date = date;
+		this.valorHora = valorHora;
+		this.horas = horas;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(Double valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public Integer getHoras() {
+		return horas;
+	}
+
+	public void setHoras(Integer horas) {
+		this.horas = horas;
+	}
+	
+	
+}
