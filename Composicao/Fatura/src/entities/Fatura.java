982300dffe4de3bd755c1907c652168154c71936
Lucas Menduiña <mynameis_menduina@outlook.com>
2023-06-14 20:17:46 -0300

    feature: add fatura class with an ArrayList of itens - (composition)

---
+++ Composicao/Fatura/src/entities/Fatura.java
@@ -0,0 +1,35 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fatura {
+	public String nome ; 
+	public String cpf ; 
+	
+	public Fatura() {
+	}
+	
+	public Fatura(String nome, String cpf) {
+		this.nome = nome ; 
+		this.cpf = cpf ; 
+	}
+	
+	List<Item> ItemList = new ArrayList<Item>() ; 
+	
+	public void setNome(String nome) {
+		this.nome = nome ; 
+	}
+	
+	public String getNome() {
+		return this.nome ;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf ; 
+	}
+	
+	public String getCpf() {
+		return this.cpf ; 
+	}
+}
