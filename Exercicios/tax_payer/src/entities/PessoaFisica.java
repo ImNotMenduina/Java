9e4ba78c3ce5a157d0df37152197b1f6c4a78a07
Lucas Menduiña <mynameis_menduina@outlook.com>
2023-06-15 11:24:19 -0300

    feature: PessoaFisica and PessoaJuridica classes and their methods to calculate taxes

--- Exercicios/tax_payer/src/entities/PessoaFisica.java
+++ Exercicios/tax_payer/src/entities/PessoaFisica.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa{
+	private Double gastosSaude ; 
+	
+	public PessoaFisica() {
+	}
+	
+	public PessoaFisica(String nome, Double rendaAnual, Double gastosSaude) {
+		super(nome, rendaAnual) ; 
+		this.gastosSaude = gastosSaude ; 
+	}
 
+	@Override
+	public Double impostoPago() {
+		Double impostoPago = 0.0; 
+		
+		if(this.rendaAnual < 2000.0) {
+			impostoPago = rendaAnual*0.15 ; 
+		} else {
+			impostoPago = rendaAnual*0.25 ; 
+		}
+		
+		if(gastosSaude > 0.0) {
+			impostoPago -= gastosSaude*0.5 ; 
+		}
+		return impostoPago ;
+	}
+	
+	
 }
--- Exercicios/tax_payer/src/entities/PessoaJuridica.java
+++ Exercicios/tax_payer/src/entities/PessoaJuridica.java
@@ -1,5 +1,28 @@
 package entities;
 
-public class PessoaJuridica {
-
+public class PessoaJuridica extends Pessoa{
+	private Integer numeroFuncionarios ; 
+	
+	public PessoaJuridica() {
+	}
+	
+	public PessoaJuridica(String nome, Double rendaAnual, Integer numeroFuncionarios) {
+		super(nome, rendaAnual); 
+		this.numeroFuncionarios = numeroFuncionarios ; 
+	}
+	
+	public void setNumeroFuncionarios(Integer numeroFuncionarios) {
+		this.numeroFuncionarios = numeroFuncionarios ; 
+	}
+	
+	public Integer getNumeroFuncionarios() {
+		return this.numeroFuncionarios ; 
+	}
+	
+	public Double impostoPago() {
+		if(numeroFuncionarios > 10) {
+			return rendaAnual*0.14 ; 
+		}
+		return rendaAnual*0.16 ; 
+	}
 }
